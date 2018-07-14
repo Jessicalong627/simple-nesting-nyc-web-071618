118b92f14973632a623ee1b3282187d4bbd716fa
Jessica Long <jessicalong627@gmail.com>
2018-07-14 23:23:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
+  programmer_hash[:dennis_ritchie][:languages] = alans_new_info
 end
